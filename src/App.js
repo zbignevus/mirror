49a160463ad672299f329bcdf573c974b80030c0
unknown <zbignevus@gmail.com>
2020-02-20 12:51:29 +0200

    Added Routing & HomePage with initial layout scss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import HomePage from './pages/homepage/homepage.component';
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path='/' component={HomePage}/>
+    </Switch>
+  </Router>
+)
 
 export default App;
